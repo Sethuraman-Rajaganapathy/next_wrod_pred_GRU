ef6f93e98f224ed147a9f7d3a6526ec126198425
Sethuraman-Rajaganapathy <sethuramanrajaganapathy94@gmail.com>
2024-10-06 23:08:51 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pickle
-import tensorflow
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
